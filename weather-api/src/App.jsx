4a2b8c30d448eeccd4db4f935205e66eed8a5607
Richarlison mendonça <dev.richarlison@gmail.com>
2025-04-16 13:43:48 -0300

    Adding Ux

--- weather-api/src/App.jsx
+++ weather-api/src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { Search } from 'lucide-react';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const inputRef = useRef()
+  const apiKey = '4a2ba0f48e7dae6a5249c3b53ee1b70d'
+  function city(){
+    console.log(inputRef.current.value)
 
+  }
   return (
     <>
       <div className="main_section">
         <header>
-          <h1>WEATHER <span> NOW</span></h1>
+          <h1>WEATHER 
+            <span> NOW</span>
+            </h1>
         </header>
-        <div className="api_search">
+        
+        <div 
+        className="api_search">
           <Search size={20} />
-          <input type="text"  id="text_user" placeholder='Enter a Place:  '/>
-          <button>Enter</button>
+          
+          <input
+           type="text"  
+           id="text_user" 
+           ref={inputRef}
+           placeholder='Enter a Place:'
+           />
+
+          <button onClick={city}>Enter</button>
         </div>
       </div>
     </>
