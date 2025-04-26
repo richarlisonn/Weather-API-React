cfbf3f9929b08a7033d6c8670e0435a696feb116
Richarlison mendonça <dev.richarlison@gmail.com>
2025-04-26 01:58:25 -0300

    Creating input error message if it doesn't work correctly

--- weather-api/src/App.jsx
+++ weather-api/src/App.jsx
@@ -5,18 +5,33 @@ import WeatherInformation from './components/weather-info/WeatherInformation';
 
 function App() {
 
+ 
+  
   const [weather, setWeather] = useState()
 
   const inputRef = useRef()
 
   async function apiService(){
+
     const cityName = inputRef.current.value
     const apiKey = '4a2ba0f48e7dae6a5249c3b53ee1b70d'
     const urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&lang=pt_br&units=metric`
 
     const apiInfo = await axios.get(urlApi)
-    setWeather(apiInfo.data)
+
+    //error
+    if(inputRef == "") {
+      alert('Please enter a city name')
+      
+    }else{
+      setWeather(apiInfo.data)
+    }
+
   }
+
+  
+
+  
 
   return (
     <>
